2d056b55319283a6d5d7a9514c3c8fcc064018e7
Praveenkarmegam <lovelypraveendp@gmail.com>
2026-08-05 21:13:19 +0530

    data updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import dns from "dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://praveen:praveen@nearbyhunt.ak8z7.mongodb.net/food-delivery").then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB Connected"));
 }
 
 
